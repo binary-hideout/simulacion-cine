942f55648d6c873416a8a7a24693235488be42e4
netotz <neto.otz@hotmail.com>
2020-06-12 00:32:36 -0500

    Agregar formateo de hora para tiempo ocio total

--- sistema.py
+++ sistema.py
@@ -1,17 +1,12 @@
 import tkinter as tk
 import tkinter.messagebox
+
 from flujo import simular
 from validaciones import formatear_hora
 from validaciones import validar_pcl
 from validaciones import validar_tlim
 
-# root = tk.Tk()
-# hello_message = tk.Label(root, text="Hello World!")
-# hello_message.pack()
-# root.mainloop()
-
 class Sistema:
-
     def __init__(self, master):
         self.frame = tk.Frame(master, width=800, height=500)
         self.frame.pack()
@@ -28,7 +23,7 @@ class Sistema:
 
         self.clientes_entry = tk.Entry(self.frame)
         self.clientes_entry.place(x=200, y=150)
-        
+
         # Salida caja 1
         self.tiempo_caja_1 = tk.Label(self.frame, text="Tiempo de ocio caja #1")
         self.tiempo_caja_1.place(x=400, y=0)
@@ -57,7 +52,7 @@ class Sistema:
 
         self.salida_clientes_caja_2 = tk.StringVar()
         self.clientes_caja_2_text = tk.Label(self.frame, textvariable=self.salida_clientes_caja_2, fg="blue")
-        self.clientes_caja_2_text.place(x=600, y=200)        
+        self.clientes_caja_2_text.place(x=600, y=200) 
 
         # Salida caja 3
         self.tiempo_caja_3 = tk.Label(self.frame, text="Tiempo de ocio caja #3")
@@ -72,10 +67,10 @@ class Sistema:
 
         self.salida_clientes_caja_3 = tk.StringVar()
         self.clientes_caja_3_text = tk.Label(self.frame, textvariable=self.salida_clientes_caja_3, fg="blue")
-        self.clientes_caja_3_text.place(x=600, y=350)        
+        self.clientes_caja_3_text.place(x=600, y=350)
 
         # Salida clientes
-        self.clientes_total = tk.Label(self.frame, text="Clientes atendidos")
+        self.clientes_total = tk.Label(self.frame, text="Total de clientes atendidos")
         self.clientes_total.place(x=400, y=400)
 
         self.salida_clientes = tk.StringVar()
@@ -93,7 +88,7 @@ class Sistema:
 
         self.button = tk.Button(self.frame, text="CORRER", fg="blue", command=self.correr_simulacion)
         self.button.place(x=50, y=400)
-    
+
     def correr_simulacion(self):
         tiempo_limite = self.minutos_entry.get()
         cantidad_clientes = self.clientes_entry.get()
@@ -103,13 +98,14 @@ class Sistema:
             cantidad_clientes = int(cantidad_clientes)
         else:
             self.error = tkinter.messagebox.showerror(title="ERROR", message="Verificar entrada")
+            return
 
-        to, na, tna = simular(tiempo_limite, cantidad_clientes)
-        
-        self.tiempo_ocio_total = float(to[0]) + float(to[1]) + float(to[2])
-        print(float(to[0]))
-        print(float(to[1]))
-        print(float(to[2]))
+        to, na = simular(tiempo_limite, cantidad_clientes)
+
+        self.tiempo_ocio_total = sum(to)
+        print(to[0])
+        print(to[1])
+        print(to[2])
 
         self.salida_caja_1.set(formatear_hora(to[0]))
         self.salida_caja_2.set(formatear_hora(to[1]))
@@ -119,9 +115,8 @@ class Sistema:
         self.salida_clientes_caja_2.set(str(na[1]) + " clientes")
         self.salida_clientes_caja_3.set(str(na[2]) + " clientes")
 
-        self.salida_clientes.set(str(tna) + " clientes")
-        self.salida_tiempos.set(self.tiempo_ocio_total)
-
+        self.salida_clientes.set(str(sum(na)) + " clientes")
+        self.salida_tiempos.set(formatear_hora(self.tiempo_ocio_total))
 
 root = tk.Tk()
 root.title("Cinema Queue Simulator PRO")
