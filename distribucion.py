d093dfb867e17f0daea5d3cd374904d3c2dbfdf0
netotz <neto.otz@hotmail.com>
2020-06-12 00:31:03 -0500

    Agregar prevención de negativos

--- distribucion.py
+++ distribucion.py
@@ -3,4 +3,4 @@ from random import random
 def distribucion(sd: float, m: float) -> float:
     suma = sum(random() for _ in range(12))
     normal = m + (sd * (suma - 6))
-    return normal
+    return normal if normal > 0 else distribucion(sd, m)
